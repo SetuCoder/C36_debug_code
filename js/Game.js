acd4b0b8926a3141fc19b7a5cb721384f9a8dfe4
SetuCoder <58362876+SetuCoder@users.noreply.github.com>
2020-04-17 09:08:32 +0530

    Add files via upload

---
+++ js/Game.js
@@ -0,0 +1,50 @@
+class Game {
+  constructor(){}
+  
+  getState(){
+    var gameStateRef  = database.ref('gameState');
+    gameStateRef.on("value",function(data){
+       gameState = data.val();
+    })
+   
+  }
+
+  update(state){
+    database.ref('/').update({
+      gameState: state
+    });
+  }
+
+  play(){
+    form.hide();
+    text("Game Start",120,100);
+    Player.getPlayerInfo();
+    if(allPlayers!==undefined){
+      var displayPosition = 130;
+      for(var plr in allPlayers){
+        if(plr === "player"+player.index)
+        fill("red")
+        else
+        fill("black")
+        displayPosition+=20
+        textSize(15)
+        text(allPlayers[plr].name+":"+allPlayers[plr].distance,120,displayPosition)
+      }
+    }
+    if(keyIsDown(UP_ARROW)&&player.index!==null){
+      player.distance+=50
+      player.update();
+    }
+  }
+
+  start(){
+    if(gameState === 0){
+      player = new Player();
+      player.getCount();
+      form = new Form()
+      form.display();
+    }
+  }
+}
+
+
---
+++ js/Player.js
@@ -0,0 +1,38 @@
+class Player {
+  constructor(){
+    this.index = null;
+    this.distance = 0;
+    this.name = null;
+  }
+
+  getCount(){
+    var playerCountRef = database.ref('playerCount');
+    playerCountRef.on("value",function(data){
+      playerCount = data.val();
+    })
+  }
+
+  updateCount(count){
+    database.ref('/').update({
+      playerCount: count
+    });
+  }
+
+  update(){
+    var playerIndex = "players/player" + this.index;
+    database.ref(playerIndex).set({
+      name:this.name,
+      distance:this.distance
+    });
+  }
+
+
+//static func is called by class not by object
+  static getPlayerInfo(){
+    var playerInforef = database.ref('players');
+    playerInforef.on("value",(data)=>{
+      allPlayers = data.val();
+    })
+  }
+
+}
